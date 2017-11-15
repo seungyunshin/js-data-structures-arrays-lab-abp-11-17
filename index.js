0d5736707d60ffabdc906b1dea64861424faa1a5
Seung-Yun <sseungyun@gmail.com>
2017-11-15 06:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyPrependDriver(){
 }
 
 function destructivelyPrependDriver(){
-  return drivers.pop('')
+  return drivers.pop()
 }
 
 function destructivelyRemoveFirstDriver(){
-  return drivers.shift('')
+  return drivers.shift()
 }
