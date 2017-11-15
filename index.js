6d96d5163c1cbd7bd08f6fd81d5c7e0cfbbb9608
Seung-Yun <sseungyun@gmail.com>
2017-11-15 06:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(){
   return drivers.push('Ralph')
 }
+
+function destructivelyPrependDriver(){
+  return drivers.unshift('Bob')
